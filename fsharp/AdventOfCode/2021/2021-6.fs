a980ea107504db849a81e813b22f006d37748148
MAV <gitkraken@m-a-v.dk>
2021-12-06 23:15:37 +0100

    Solved Day 6 Part 1 & 2

--- fsharp/AdventOfCode/2021/2021-6.fs
+++ fsharp/AdventOfCode/2021/2021-6.fs
@@ -1,13 +1,42 @@
 ﻿module _2021_6
 
-// Task 1: 
-// Task 2: 
+// Task 1: Count multiplying fish after 80 days
+// Task 2: Count them after 256 days (exponential so naive solution wont work)
 
+let parseInitialState (line:string) =
+    line.Split(',') |> Array.map (fun x -> int x)
+
+let rec calcStateAfterDays state days : (int * int64) list=
+    let processGroup state : (int * int64) list = 
+        match state with 
+        | (0, count) -> [ (6, count); (8, count) ]
+        | (i, count) -> [ (i - 1, count) ]
+
+    match days with 
+    | 0 -> state
+    | x -> calcStateAfterDays 
+                // Here we just re-group to merge duplicate groups. We could probably have made a solution that
+                // just updated the existing group. But whatevs.. This is fast enough and it works
+                (state |> 
+                    List.map processGroup |> 
+                    List.collect id |>
+                    List.groupBy fst |> 
+                    List.map (fun (key, items) -> (key, items |> List.sumBy snd))) 
+                (days - 1)     
+
+    
 let execute (input : string seq) =
-    printfn "Input count: %i" (Seq.length input)
+    let initialState : (int * int64) list = 
+        parseInitialState (input |> Seq.head) |> 
+        List.ofArray |>
+        List.countBy id |>
+        List.map (fun (day, count) -> (day, int64 count))
 
-    let part1 = "N/A"
+    //printfn "%A" initialState
 
-    let part2 = "N/A"
+    printfn "Initial states: %i" (List.length initialState)
+
+    let part1 = calcStateAfterDays initialState 80 |> List.map snd |> List.sum
+    let part2 = calcStateAfterDays initialState 256 |> List.map snd |> List.sum
 
     part1.ToString(), part2.ToString()
