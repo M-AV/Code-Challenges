4509e56a4619e8c1e511d4dd4c4c9cfebfb81070
MAV <gitkraken@m-a-v.dk>
2021-12-07 18:06:45 +0100

    Solved Day 7 Part 1 & 2

--- fsharp/AdventOfCode/2021/2021-7.fs
+++ fsharp/AdventOfCode/2021/2021-7.fs
@@ -1,13 +1,49 @@
 ﻿module _2021_7
 
-// Task 1: 
-// Task 2: 
+// Task 1: Find minimum number of adjustments crabs need to make to be aligned
+// Task 2: Find minimum number but fuel is exponential
+
+let parseInitialState (line:string) =
+    line.Split(',') |> Array.map int
+
+let findMinimumMoves_ConstantFuelBurn (positions:int list) =
+    let fuelNeeded positions target =
+        positions |> 
+        List.map (fun x -> (max target x) - (min target x)) |> 
+        List.sum
+
+    let lowest = List.min positions
+    let highest = List.max positions
+
+    [ for i in lowest .. highest -> i ] |>
+        List.map (fun x -> fuelNeeded positions x) |>
+        List.min
+
+let findMinimumMoves_ExponentialFuelBurn(positions:int list) =
+    // Formula: 0.5 * n * (n + 1)
+    let fuelNeeded positions target =
+        positions |> 
+        List.map (fun position -> (max target position) - (min target position)) |>
+        List.map (fun moves -> (moves * (moves + 1)) >>> 1) |>
+        List.sum
+
+    let lowest = List.min positions
+    let highest = List.max positions
+
+    [ for i in lowest .. highest -> i ] |>
+        List.map (fun x -> fuelNeeded positions x) |>
+        List.min
 
 let execute (input : string seq) =
-    printfn "Input count: %i" (Seq.length input)
+    
+    let parsed = 
+        input |> 
+        Seq.map parseInitialState |> List.ofSeq |> List.head |> List.ofSeq |> List.sort
 
-    let part1 = "N/A"
+    printfn "Input count: %i" (Seq.length parsed)
 
-    let part2 = "N/A"
+    let part1 = findMinimumMoves_ConstantFuelBurn parsed
+
+    let part2 = findMinimumMoves_ExponentialFuelBurn parsed
 
     part1.ToString(), part2.ToString()
