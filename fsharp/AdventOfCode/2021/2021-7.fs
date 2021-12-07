a157664636b9f6720b55e17aef76c85c7390d920
MAV <gitkraken@m-a-v.dk>
2021-12-07 18:19:58 +0100

    Merged Day 7 part 1 & 2 functions
    Only difference was the fuel cost function, which could easily be a parameter

--- fsharp/AdventOfCode/2021/2021-7.fs
+++ fsharp/AdventOfCode/2021/2021-7.fs
@@ -6,25 +6,11 @@
 let parseInitialState (line:string) =
     line.Split(',') |> Array.map int
 
-let findMinimumMoves_ConstantFuelBurn (positions:int list) =
+let findMinimumMoves positions fuelCostFunction =
     let fuelNeeded positions target =
         positions |> 
         List.map (fun x -> (max target x) - (min target x)) |> 
-        List.sum
-
-    let lowest = List.min positions
-    let highest = List.max positions
-
-    [ for i in lowest .. highest -> i ] |>
-        List.map (fun x -> fuelNeeded positions x) |>
-        List.min
-
-let findMinimumMoves_ExponentialFuelBurn(positions:int list) =
-    // Formula: 0.5 * n * (n + 1)
-    let fuelNeeded positions target =
-        positions |> 
-        List.map (fun position -> (max target position) - (min target position)) |>
-        List.map (fun moves -> (moves * (moves + 1)) >>> 1) |>
+        List.map fuelCostFunction |>
         List.sum
 
     let lowest = List.min positions
@@ -42,8 +28,9 @@ let execute (input : string seq) =
 
     printfn "Input count: %i" (Seq.length parsed)
 
-    let part1 = findMinimumMoves_ConstantFuelBurn parsed
+    let part1 = findMinimumMoves parsed id
 
-    let part2 = findMinimumMoves_ExponentialFuelBurn parsed
+    // Formula: 0.5 * n * (n + 1)
+    let part2 = findMinimumMoves parsed (fun x -> (x * (x + 1)) >>> 1)
 
     part1.ToString(), part2.ToString()
