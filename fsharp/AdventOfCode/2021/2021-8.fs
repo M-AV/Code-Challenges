d6ce11ce8eed502e870e4f8a1d2a0a09288201c2
MAV <gitkraken@m-a-v.dk>
2021-12-08 21:00:15 +0100

    Solved Day 8 Part 1

--- fsharp/AdventOfCode/2021/2021-8.fs
+++ fsharp/AdventOfCode/2021/2021-8.fs
@@ -1,12 +1,27 @@
 ﻿module _2021_8
 
-// Task 1: 
+// Task 1: Count words in output with length 2, 3, 4 or 7
 // Task 2: 
+
+let parseInput_Part1 (input : string seq) =
+    input 
+    |> Seq.map (fun x -> x.Split(" | "))
+    |> Seq.map (fun x -> x[1])
+    |> Seq.map (fun x -> x.Split(' '))
 
 let execute (input : string seq) =
     printfn "Input count: %i" (Seq.length input)
 
-    let part1 = "N/A"
+    // This is the simplest possible way I can think of to solve part one. It makes no attempt at parsing
+    // the values which I assume we need to do in part 2.
+    let part1 = 
+        input 
+        |> parseInput_Part1 
+        |> Seq.collect id
+        |> Seq.filter (fun x -> (Array.contains x.Length [|2; 4; 3; 7|])) 
+        |> Seq.length
+
+    printfn "%A" part1
 
     let part2 = "N/A"
 
