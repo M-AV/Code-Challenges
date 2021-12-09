d5a5109fb656e02e464ee6afe1b54825745c2269
MAV <gitkraken@m-a-v.dk>
2021-12-10 00:39:16 +0100

    Solved Day 9 Part 2

--- fsharp/AdventOfCode/2021/2021-9.fs
+++ fsharp/AdventOfCode/2021/2021-9.fs
@@ -14,32 +14,63 @@ let parseInput (input: string seq) =
 
     (parsedLines, lineLength)
 
+
+let aboveIdx (index:int, lineLength:int) = index - lineLength
+let nextIdx index = index + 1
+let belowIdx index lineLength = index + lineLength
+let prevIdx index lineLength = index - 1
+
+let valAbove (input:int array, index:int, lineLength:int) = 
+    if index - lineLength < 0 then (-1, 99999999)
+    else (index - lineLength, input[index - lineLength])
+let valBelow(input:int array, index:int, lineLength:int) = 
+    if index + lineLength > (Array.length input) then (-1, 99999999)
+    else (index + lineLength, input[index + lineLength])
+let valPrev(input:int array, index:int, lineLength:int) =
+    if index % lineLength = 0 then (-1, 99999999)
+    else (index - 1, input[index - 1])
+let valNext(input:int array, index:int, lineLength:int) =
+    if index = 0 then (1, input[1])
+    else if (index + 1) % lineLength = 0 then (-1, 99999999)
+    else (index + 1, input[index + 1])
+
+let isLowPoint input lineLength idx value  =
+    value < snd(valAbove(input, idx, lineLength)) && 
+    value < snd(valNext(input, idx, lineLength)) && 
+    value < snd(valBelow(input, idx, lineLength)) && 
+    value < snd(valPrev(input, idx, lineLength))
+
 let countLowPoints (input:int array, lineLength:int) = 
-    let valAbove index = 
-        if index - lineLength < 0 then 99999999
-        else input[index - lineLength]
-    let valBelow index = 
-        if index + lineLength > (Array.length input) then 99999999
-        else input[index + lineLength]
-    let valPrev index =
-        if index % lineLength = 0 then 99999999
-        else input[index - 1]
-    let valNext index =
-        if index = 0 then input[1]
-        else if (index + 1) % lineLength = 0 then 99999999
-        else input[index + 1]
-    let isLowPoint idx value =
-        value < valAbove(idx) && 
-        value < valNext(idx) && 
-        value < valBelow(idx) && 
-        value < valPrev(idx)
-        
-        
     input 
     |> Seq.indexed
-    |> Seq.filter (fun (idx, x) -> isLowPoint idx x)
+    |> Seq.filter (fun (idx, x) -> isLowPoint input lineLength idx x)
     |> Seq.map (fun (_, x) -> x + 1)
     |> Seq.sum
+
+let findBasins (input: int array, lineLength:int) =
+    let rec findBasin idx prevIdx result : int Set =
+        if Set.contains idx result then Set.empty // We only do this to avoid going on paths already visited (even though the set would remove duplicates)
+        else
+            match idx with
+            | -1 -> Set.empty
+            | x when input[idx] = 9 -> Set.empty
+            | x when prevIdx >= 0 && input[idx] <= input[prevIdx] -> Set.empty
+            | _ ->  let result = Set.empty.Add(idx)
+                    result 
+                    |> Set.union (findBasin (fst(valAbove(input, idx, lineLength))) idx result)
+                    |> Set.union (findBasin (fst(valNext(input, idx, lineLength))) idx result)
+                    |> Set.union (findBasin (fst(valBelow(input, idx, lineLength))) idx result)
+                    |> Set.union (findBasin (fst(valPrev(input, idx, lineLength))) idx result)
+
+    let lowPoints =
+        input 
+        |> Seq.indexed
+        |> Seq.filter (fun (idx, x) -> isLowPoint input lineLength idx x)
+        |> Seq.map fst
+
+    //printfn "%A" (lowPoints |> List.ofSeq)
+
+    lowPoints |> Seq.map (fun x -> findBasin x -1 Set.empty)
 
 let execute (input : string seq) =
     printfn "Input count: %i" (Seq.length input)
@@ -50,6 +81,13 @@ let execute (input : string seq) =
 
     let part1 = countLowPoints (values, lineLength)
 
-    let part2 = "N/A"
+    let basins = findBasins (values, lineLength)
+
+    let part2 = 
+        basins 
+        |> Seq.sortByDescending Seq.length 
+        |> Seq.take 3 
+        |> Seq.map Seq.length 
+        |> Seq.reduce (fun agg x -> agg * x)
 
     part1.ToString(), part2.ToString()
