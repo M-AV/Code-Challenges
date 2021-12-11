c736b875155a441b344aa7d5566ab4bb7d6dfa96
MAV <gitkraken@m-a-v.dk>
2021-12-11 15:17:56 +0100

    Solved Day 10 Part 2

--- fsharp/AdventOfCode/2021/2021-10.fs
+++ fsharp/AdventOfCode/2021/2021-10.fs
@@ -3,10 +3,10 @@
 // Task 1: Ignore incomplete lines, find first invalid paranthesis and calculate score. Sum scores for result
 // Task 2: 
 
-let openBrackets = [ '['; '('; '<'; '{' ]
-let closeBrackets = [ ']'; ')'; '>'; '}' ]
+let openBrackets = [| '['; '('; '<'; '{' |]
+let closeBrackets = [| ']'; ')'; '>'; '}' |]
 
-let pointLookup char = 
+let corruptedPointLookup char = 
     match char with
     | ')' -> 3
     | ']' -> 57
@@ -14,20 +14,45 @@ let pointLookup char =
     | '>' -> 25137
     | _ -> 0
 
+let missingPointLookup char =
+    match char with
+    | ')' -> 1L
+    | ']' -> 2L
+    | '}' -> 3L
+    | '>' -> 4L
+    | _ -> 0
+
 let isOpenBracket bracket = List.contains bracket  [ '['; '('; '<'; '{' ]
 let isCloseBracket bracket = List.contains bracket [ ']'; ')'; '>'; '}' ]
-let matches (first:char) (second:char) = (openBrackets |> List.findIndex (fun x -> x = first)) = (closeBrackets |> List.findIndex (fun x -> x = second) )
+let matches (first:char) (second:char) = (openBrackets |> Array.findIndex (fun x -> x = first)) = (closeBrackets |> Array.findIndex (fun x -> x = second) )
+let getMatchingClose first = closeBrackets[openBrackets |> Array.findIndex (fun x -> x = first)]
 
 let findFirstCorruptedChar (input: char list): char =
     let rec traverse input queue = 
         match input with 
         | head :: tail when isOpenBracket head -> traverse tail (head :: queue)
-        | head :: tail when List.isEmpty queue -> head
+        | head :: tail when List.isEmpty queue -> head // If empty and we see a close bracket it's corrupted
         | head :: tail when head |> matches queue.Head -> traverse tail queue.Tail
         | [] -> '&' // Just return a random char for uncorrupted lines
         | head :: tail -> head
     
     traverse input []
+
+let findMissingBrackets (input: char list): char list =
+    let rec findOpen queue input : char list =
+        match input with 
+        | head :: tail when isOpenBracket head -> findOpen (head :: queue) tail
+        | head :: tail when List.isEmpty queue -> []
+        | head :: tail when head |> matches queue.Head -> findOpen queue.Tail tail
+        | [] -> queue 
+        | head :: tail -> [] // Just return empty list for corrupted lines
+
+    input |> findOpen [] |> List.map getMatchingClose
+
+let calcMissingBracketScore (input: char list) =
+    input 
+    |> List.map missingPointLookup
+    |> List.reduce (fun agg x -> agg * 5L + x)
 
 let execute (input : string seq) =
     printfn "Input count: %i" (Seq.length input)
@@ -36,8 +61,17 @@ let execute (input : string seq) =
         input 
         |> Seq.map List.ofSeq
         |> Seq.map findFirstCorruptedChar
-        |> Seq.sumBy pointLookup
+        |> Seq.sumBy corruptedPointLookup
 
-    let part2 = "N/A"
+    let points = 
+        input 
+        |> Seq.map List.ofSeq
+        |> Seq.map findMissingBrackets
+        |> Seq.filter (fun x -> not (Seq.isEmpty x))
+        |> Seq.map calcMissingBracketScore
+        |> Seq.sort
+        |> Array.ofSeq
+
+    let part2 = points[points.Length / 2]
 
     part1.ToString(), part2.ToString()
