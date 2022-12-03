4fdbb1cfd45ca1f1a3a4fffefb47974dad0deb32
MAV <gitkraken@m-a-v.dk>
2022-12-03 20:27:11 +0100

    Solved Day 3

--- fsharp/AdventOfCode/2022/2022-03.fs
+++ fsharp/AdventOfCode/2022/2022-03.fs
@@ -1,16 +1,49 @@
 module _2022_3
 
 open InputProvider
-open Calculations
 open Parsing
-open System
 open Xunit
+open System.Linq
 
-// Task 1: 
-// Task 2: 
+// Task 1: Find duplicate letter in each half of each string and sum up the values (a-z = 1-26, A-Z=27-52)
+// Task 2: Find common item in each set of 3 lines and sum like before
 
 let parseInput (input : string seq) = 
     input
+
+let convertToPoints letter =
+    match (int letter) with
+    | i when i < 97 -> i - 38
+    | x -> x - 96
+
+let part1 (input : string seq) =
+    let findDuplicate str =
+        let middle = Array.length str / 2
+
+        let first = str |> Seq.take middle;
+        let second = str |> Seq.skip middle;
+
+        let inter = Enumerable.Intersect(first, second) |> Seq.exactlyOne
+
+        inter
+
+    input 
+    |> Seq.map Array.ofSeq
+    |> Seq.map findDuplicate
+    |> Seq.map convertToPoints
+    |> Seq.sum
+
+let part2 input =
+    let sum = 
+        input
+        |> batchesOf 3
+        // Not sure this is the best way of taking the first 3 items..
+        |> Seq.map (fun x -> (x |> Seq.take 1 |> Seq.exactlyOne, x |> Seq.skip 1 |> Seq.take 1 |> Seq.exactlyOne, x |> Seq.skip 2 |> Seq.take 1 |> Seq.exactlyOne))
+        |> Seq.map (fun (x,y,z) -> Enumerable.Intersect(Enumerable.Intersect(x , y), z) |> Seq.exactlyOne)
+        |> Seq.map convertToPoints
+        |> Seq.sum
+
+    sum
 
 let execute (input : string seq) =
     printfn "Input count: %i" (Seq.length input)
@@ -18,14 +51,14 @@ let execute (input : string seq) =
     let parsed = parseInput input
     printfn "%A" parsed
 
-    let part1 = "N/A"
+    let part1 = part1 parsed
 
-    let part2 = "N/A"
+    let part2 = part2 parsed
 
     part1.ToString(), part2.ToString()
 
-//[<Fact>]
+[<Fact>]
 let ``Test``() =
     let (part1, part2) = execute (getPuzzleInput "2022" "3" |> Async.RunSynchronously)
-    Assert.Equal("N/A", part1)
-    Assert.Equal("N/A", part2)
+    Assert.Equal("8394", part1)
+    Assert.Equal("2413", part2)
--- fsharp/AdventOfCode/Utils/Parsing.fs
+++ fsharp/AdventOfCode/Utils/Parsing.fs
@@ -1,10 +1,20 @@
 ﻿module Parsing
 
+let trd (_, _, c) = c
+
 let charToInt ch = (int ch) - (int '0')
-let ints (input:string) = input |> Seq.map charToInt |> Array.ofSeq
+let ints (input:string) = 
+    input |> Seq.map charToInt |> Array.ofSeq
 let parseArray2d (input: string seq) =
     let arr = 
         input
         |> Seq.map Array.ofSeq
         |> Array.ofSeq
     Array2D.init arr[0].Length arr.Length (fun x y -> arr[y][x])
+
+// Batching logic from here: https://stackoverflow.com/a/7518857/1401257
+let batchesOf n = 
+    Seq.mapi (fun i v -> i / n, v) >>
+    Seq.groupBy fst >>
+    Seq.map snd >>
+    Seq.map (Seq.map snd)
