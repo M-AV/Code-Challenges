ab1ac76bd0263cb825dbecfa7a903d67b52e3d00
MAV <gitkraken@m-a-v.dk>
2022-12-07 18:25:36 +0100

    I redeemed myself
    I looked around on the AoC subreddit and someone mentioned a "flat file structure" which inspired me to make the solution below. The parsing got MUCH simpler this way.

--- fsharp/AdventOfCode/2022/2022-07.fs
+++ fsharp/AdventOfCode/2022/2022-07.fs
@@ -9,126 +9,92 @@ open Xunit
 // NONE OF THIS WORKS!
 
 // Task 1: Parse folder structure and find every folder with size at 100000 or less and sum it.
-// Task 2:
+// Task 2: Find smallest folder to delete so we get enough space
 
-type File = int * string
+// The idea here is to parse everything into a flat file structure where each directory name includes the parent name.
+// To find sub-folders we should then simply be able to check if the name starts with the parent name
 
-type Node =
-    | File of int * string
-    | Dir of string * Map<string, Node> * Option<Node>
+let parseInput (input : string seq) = 
+    let parseFile (line:string) =
+        let split = line.Split ' '
+        
+        (split[0] |> int, split[1])
+        
 
-//let getDir (node:Node) =
-//    match node with
-//    | Dir(n,c) -> Dir(n,c)
+    let rec parse (input:string list) (currentPath:string) (folders:(string * int) list)=
+        match input with
+        | [] -> folders
+        | head::tail -> 
+            match head with 
+            | Prefix "$ cd .." rest -> 
+                let lastSlash = currentPath |> Seq.findIndexBack(fun x -> x = '/')
+                let parentDir = currentPath[0..lastSlash - 1]
+                parse tail parentDir folders
+            | Prefix "$ cd " rest ->
+                let newDir = currentPath + "/" + rest
+                parse tail newDir folders
+            | Prefix "$ ls" rest -> 
+                let files = 
+                    tail |> List.takeWhile (fun x -> not(x.StartsWith("$"))) 
+                    
+                let fileSize = files |> List.filter (fun x -> not(x.StartsWith("dir"))) |> List.map parseFile |> List.sumBy fst
+                let remaining = tail |> List.skip files.Length
+                let updatedFolders = ((currentPath, fileSize)::folders)
 
-let parseInput (input : string seq): Node = 
-    input
-    //let parseFile line =
-    //    let split = file.Split ' '
-    //    File(split[0] |> int, split[1])
+                parse remaining currentPath updatedFolders
 
-    //let rec parse2 remanining (current:Node) : Node=
-    //    match remanining with
-    //    | [] -> current
-    //    | head::tail ->
-    //        match head with 
-    //        | "" -> current
-    //        | Prefix "$ cd " rest ->
-    //            match current with
-    //            | Dir(name, children, parent) ->
-    //                let d = Dir(name, children |> Map.add rest parsedDir, parent)
-    //                parent.Value <- d
-    //                parse tail parsed d
-    //            let ch = current
-    //            //let folder = parse2 tail 
-    //            current
-    //        | Prefix "$ ls" rest -> 
-    //            let toProcess = tail |> List.takeWhile(fun x -> !x.StartsWith('$'))
-    //            let left = tail |> List.skip toProcess.Length
+    parse (input |> Seq.skip 1 |> List.ofSeq) "" []
 
-    //            let processed = 
-    //                toProcess 
-    //                |> List.filter (fun x -> !(x.StartsWith "dir ")) 
-    //                |> List.iter parseFile
-    //                |> List.map (fun (size, name) -> (name, File(size, name)))
+let rec calcSpaceByFolder (folder:string * int) (all:(string * int) list) =
+    let size = 
+        all 
+        |> List.filter (fun x -> (fst x).StartsWith(fst folder))
+        |> List.sumBy (fun x -> snd x)
+
+    size
+
+let part1 (input:(string * int) list) =
+    let rec solve (remaining:(string * int) list) (input:(string * int) list) =
+        match remaining with
+        | [] -> 0
+        | head::tail ->
+            let size = calcSpaceByFolder head input
+
+            if (size <= 100000) then
+                size + (solve tail input)
+            else 
+                solve tail input
+
+    solve input input
 
 
-                
-    //        | Prefix "dir " rest -> 
-    //            Dir(rest, Map.empty, Some(current))
-    //        | file ->
-    //            let split = file.Split ' '
-    //            File(split[0] |> int, split[1])
+let part2 input =
+    let totalSpace = 70000000
+    let spaceNeeded = 30000000
+    let totalSpaceUsed = calcSpaceByFolder (input |> List.last) input
+    let toBeDeleted = spaceNeeded - (totalSpace - totalSpaceUsed)
 
-
-
-    //let rec parse remaining parsed (current:Node)=
-    //    match remaining with 
-    //    | [] -> parsed
-    //    | head::tail -> 
-    //        match head with
-    //        | Prefix "$ cd /" rest -> 
-    //            let treeNode = Dir("/", Map.empty, None)
-    //            parse tail treeNode treeNode
-    //        | Prefix "$ cd .." rest ->
-    //            match current with
-    //            | Dir(_, _, parent) -> parse tail parsed parent.Value
-    //        | Prefix "$ cd " rest -> 
-    //            // Current has to be a directory, so we don't bother checking for File
-    //            match current with
-    //            | Dir(name, children, parent) -> parse tail parsed children[rest]
-    //        | Prefix "$ ls" rest ->
-    //            parse tail parsed current
-    //        | Prefix "dir " rest ->
-    //            let parsedDir = Dir(rest, Map.empty, Some(current))
-    //            match current with
-    //            | Dir(name, children, parent) ->
-    //                let d = Dir(name, children |> Map.add rest parsedDir, parent)
-    //                parent.Value <- d
-    //                parse tail parsed d
-    //        | file -> 
-    //            let split = file.Split ' '
-    //            let parsedFile = File(split[0] |> int, split[1])
-    //            match current with
-    //            | Dir(name, children, parent) -> 
-    //                let s = children |> Map.add split[1] parsedFile
-    //                let newCurrent = Dir(name, s, parent)
-    //                parse tail parsed newCurrent
-    //let root = Dir("/", Map.empty, None);
-
-    //parse (input |> List.ofSeq) root root
-
-let rec printDirectory (input:Node) depth =
-    match input with
-    | File(size, name) -> 
-        printfn "%s" (new String('-', depth) + " " + name + " (" + size.ToString() + ")")
-    | Dir(name, children, parent) ->
-        printfn "%s" (new String('-', depth) + " " + name)
-        children |> Map.iter (fun x y -> printDirectory y (depth + 1))
-
-//let part1 input = 
-//    let rec solve remaining sum =
-//        match remaining with 
-//        | [] -> sum
-//        | head::tail -> 
-//            match head with 
-//            | '
+    input 
+    |> List.map (fun x -> calcSpaceByFolder x input) 
+    |> List.filter (fun x -> toBeDeleted < x) 
+    |> List.min
+    
 
 let execute (input : string seq) =
     printfn "Input count: %i" (Seq.length input)
 
     let parsed = parseInput input
-    printDirectory parsed 1
-    //printfn "%A" parsed
 
-    let part1 = "N/A"
+    printfn "%A" parsed
 
-    let part2 = "N/A"
+    let part1 = part1 parsed
+
+    let part2 = part2 parsed
 
     part1.ToString(), part2.ToString()
 
-//[<Fact>]
+[<Fact>]
 let ``Test``() =
     let (part1, part2) = execute (getPuzzleInput "2022" "7" |> Async.RunSynchronously)
-    Assert.Equal("N/A", part1)
-    Assert.Equal("N/A", part2)
+    Assert.Equal("1477771", part1)
+    Assert.Equal("3579501", part2)
