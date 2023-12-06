f8d04202fd47583ac663998c128f07a8afa5e9e3
MAV <gitkraken@m-a-v.dk>
2023-12-06 17:27:30 +0100

    Solved day 3

--- fsharp/AdventOfCode/2023/2023-03.fs
+++ fsharp/AdventOfCode/2023/2023-03.fs
@@ -6,21 +6,90 @@ open Parsing
 open System
 open Xunit
 
-// Task 1: 
-// Task 2: 
+// Task 1: Find all number next to a symbol in the grid and sum them up
+// Task 2: Find all pairs of numbers that touches the same *. Multiply the pair and sum all of them up
 
 let parseInput (input : string seq) = 
-    input
+    input |> parsePadded2dArray '.'
+
+let isSymbol value = 
+    not (Char.IsNumber value) && value <> '.'
+
+let getNumber (grid:char array2d) (x,y)  : int = 
+    let rec parseNumber loc res =
+        match grid[fst loc, snd loc] with
+        '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> 
+            let updatedRes = res * 10 + charToInt grid[fst loc, snd loc]
+            parseNumber (fst loc + 1, snd loc) updatedRes
+        | _ -> res
+    parseNumber (x,y) 0
+
+let isNextToSymbol loc (grid:char array2d) : bool =
+    let rec traverse (x,y) isFirst =
+        if isSymbol grid[x,y-1] || isSymbol grid[x,y] || isSymbol grid[x,y+1] then
+            true
+        else if isFirst || Char.IsNumber grid[x,y] then
+            traverse (x+1, y) false
+        else
+            false
+    traverse (fst loc - 1, snd loc) true
+
+let rec getStartOfNumber (x,y) (grid:char array2d) : (int * int) =
+    if Char.IsNumber grid[x-1, y] then
+        getStartOfNumber (x-1, y) grid
+    else
+        (x,y)
+
+let getGearNumber (x,y) (grid:char array2d) : int option =
+    let mutable positions = [(x-1, y); (x+1, y); (x-1, y-1); (x-1, y+1)]
+    if not (Char.IsNumber(grid[x, y-1])) then
+        positions <- (x+1, y-1) :: positions
+    else if not (Char.IsNumber(grid[x-1, y-1])) then
+        positions <- (x, y-1) :: positions
+        
+    if not (Char.IsNumber(grid[x, y+1])) then
+        positions <- (x+1, y+1) :: positions
+    else if not (Char.IsNumber(grid[x-1, y+1])) then
+        positions <- (x, y+1) :: positions
+
+    let numbers = 
+        positions
+        |> List.map (fun (xi,yi) -> (xi,yi), Char.IsNumber grid[xi,yi])
+        |> List.filter snd
+        |> List.map (fun (loc, _) -> getStartOfNumber loc grid)
+        |> List.map (getNumber grid)
+
+    if numbers.Length < 2 then 
+        None
+    else
+        Some(numbers[0] * numbers[1])
+
 
 let part1 input =
-    0
+    let mutable numbers = []
+    for y in 1 .. Array2D.length1 input - 2 do
+        for x in 1 .. Array2D.length2 input - 2 do
+            if Char.IsNumber input[x,y] && not (Char.IsNumber input[x-1, y]) then
+                let number = getNumber input (x,y)
+                if isNextToSymbol (x,y) input then
+                    numbers <- number :: numbers
+                
+    numbers |> List.map int64 |> List.sum
 
 let part2 input =
-    0
+    let mutable numbers = []
+    for y in 1 .. Array2D.length1 input - 2 do
+        for x in 1 .. Array2D.length2 input - 2 do
+            if input[x,y] = '*' then
+                match getGearNumber (x,y) input with
+                | None -> numbers <- numbers
+                | Some x -> numbers <- x :: numbers
+                
+    numbers |> List.sum
 
 let execute (input : string seq) =
     let parsed = parseInput input
-    printfn "%A" parsed
+    printGrid2d parsed
 
     let part1 = part1 parsed
 
@@ -28,8 +97,8 @@ let execute (input : string seq) =
 
     part1.ToString(), part2.ToString()
 
-//[<Fact>]
+[<Fact>]
 let ``Test``() =
     let (part1, part2) = execute (getPuzzleInput "2023" "3" |> Async.RunSynchronously)
-    Assert.Equal("N/A", part1)
-    Assert.Equal("N/A", part2)
+    Assert.Equal("536576", part1)
+    Assert.Equal("75741499", part2)
--- fsharp/AdventOfCode/Utils/Parsing.fs
+++ fsharp/AdventOfCode/Utils/Parsing.fs
@@ -15,6 +15,20 @@ let parseArray2d (input: string seq) =
         |> Array.ofSeq
     Array2D.init arr[0].Length arr.Length (fun x y -> arr[y][x])
 
+/// Adds an extra row to top/botton and a column in beginning and end. 
+/// The extra padding can eliminate boundary checks for some challenges, making the code simpler.
+let parsePadded2dArray (paddingVal:char) (input:string seq) =
+    let parsed = parseArray2d input
+    let rows = Array2D.length1 parsed
+    let cols = Array2D.length2 parsed
+    let newArray = Array2D.init (rows + 2) (cols + 2) (fun x y -> paddingVal)
+
+    for i in 0 .. rows - 1 do
+        for j in 0 .. cols - 1 do
+            newArray[i + 1,j + 1] <- parsed[i,j]
+    
+    newArray
+
 // Logic is fairly simple, but I found this while searching for a built in way: https://stackoverflow.com/a/49891028/1401257
 let find2d needle (arr: char[,]) =
     let rec search x y =
