42b2cc9299580aa666ecab18a8030158082c9935
MAV <gitkraken@m-a-v.dk>
2023-12-06 18:18:07 +0100

    Solved day 4

--- fsharp/AdventOfCode/2023/2023-04.fs
+++ fsharp/AdventOfCode/2023/2023-04.fs
@@ -5,18 +5,44 @@ open Calculations
 open Parsing
 open System
 open Xunit
-
-// Task 1: 
-// Task 2: 
+open System.Linq
 
 let parseInput (input : string seq) = 
+    let notEmpty x = not (String.IsNullOrEmpty x)
+
     input
+    |> Seq.map (fun x -> x.Split ": ")
+    |> Seq.map (fun x -> x[1])
+    |> Seq.map (fun x -> x.Split " | ")
+    |> Seq.map (fun x -> x[0].Trim(), x[1].Trim())
+    |> Seq.map (fun (x,y) -> 
+        (x.Split " ") |> Array.filter notEmpty |> Array.map int, 
+        (y.Split " ") |> Array.filter notEmpty |> Array.map int)
+    |> List.ofSeq
 
 let part1 input =
-    0
+    input 
+    |> List.map (fun (x,y) -> (Seq.ofArray x).Intersect(Seq.ofArray y) |> List.ofSeq)
+    |> List.filter (fun x -> x.Length > 0)
+    |> List.map (fun x -> x.Length)
+    |> List.map (fun x -> Math.Pow(2.0, (float x) - 1.0))
+    |> List.sum
+
 
 let part2 input =
-    0
+    let points = 
+        input 
+        |> List.map (fun (x,y) -> (Seq.ofArray x).Intersect(Seq.ofArray y) |> List.ofSeq)
+        |> List.map (fun x -> x.Length)
+    let scratchCards = Array.init points.Length (fun _ -> 1)
+
+    // Since we can never win the first card and only the second card once etc, 
+    // we just iterate from the top and add up the cards as we go
+    for i in 0 .. (List.length points - 1) do
+        for y in i+1 .. min (i + points[i]) (scratchCards.Length-1) do
+            scratchCards[y] <- scratchCards[y] + scratchCards[i]
+
+    scratchCards |> Array.sum
 
 let execute (input : string seq) =
     let parsed = parseInput input
@@ -28,8 +54,8 @@ let execute (input : string seq) =
 
     part1.ToString(), part2.ToString()
 
-//[<Fact>]
+[<Fact>]
 let ``Test``() =
     let (part1, part2) = execute (getPuzzleInput "2023" "4" |> Async.RunSynchronously)
-    Assert.Equal("N/A", part1)
-    Assert.Equal("N/A", part2)
+    Assert.Equal("23028", part1)
+    Assert.Equal("9236992", part2)
